e2cfd29a71b904e03b1b0f8cd1249664f9d75060
juheedhone <juheedhone12@gmail.com>
2025-08-21 15:54:57 +0530

    welcome component done.

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -3,15 +3,23 @@ import { Button } from "./ui/button";
 const Welcome = () => {
   return (
     <div className="flex flex-col h-screen justify-end p-6">
-      <div className="font-semibold text-3xl pb-2">Welcome to PopX</div>
-      <p className="pb-6">
+      <div className="font-semibold text-2xl pb-2 text-[#1D2226]">
+        Welcome to PopX
+      </div>
+      <p className="pb-6 opacity-60 text-[18px] text-[#1D2226]">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
         eos.
       </p>
-      <Button variant="outline" className="bg-violet-600 text-white ">
+      <Button
+        variant="outline"
+        className="bg-[#6C25FF] text-white cursor-pointer "
+      >
         Create Account
       </Button>
-      <Button className="bg-violet-300 " variant="outline">
+      <Button
+        className="bg-[#6C25FF4B] cursor-pointer mt-2"
+        variant="outline"
+      >
         Already Register? Login
       </Button>
     </div>
